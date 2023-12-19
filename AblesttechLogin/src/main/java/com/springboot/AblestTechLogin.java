ea8063ee199442cee7c45f7ad150428eb695e0c3
yasarbhai <144100786+yasarbhai@users.noreply.github.com>
2023-12-18 21:58:04 -0800

    Add files via upload

---
+++ AblesttechLogin/src/main/java/com/springboot/AblestTechLogin.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AblestTechLogin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AblestTechLogin.class, args);
+	}
+
+}
